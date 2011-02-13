f4a98263a7398fe046d3f9d7bf60d2d035df6a7a
Kiran Ryali <kiran.ryali@gmail.com>
2011-02-13 02:12:20 -0600

    Added functionality for sockets to inject new questions

--- app.js
+++ app.js
@@ -81,7 +81,7 @@ app.get('/404', function(req, res){
 // Only listen on $ node app.js
 if (!module.parent) {
   app.listen(3000);
-  console.log("Express server listening on port %d", app.address().port)
+  console.log("Express server listening on port %d", app.address().port);
 }
 
 io = io.listen(app);
--- public/javascripts/socketconnect.js
+++ public/javascripts/socketconnect.js
@@ -1,5 +1,25 @@
-var socket = new io.Socket(null, { port: 3000, rememberTransport: false});
-socket.connect();
 
-socket.on('message', function(obj){
-});
+  var socket = new io.Socket(null, { port: 3000, rememberTransport: false});
+  socket.connect();
+
+  socket.on('message', function(obj){
+    if( obj['message'] == "question_add"){
+        var newQuestion = obj['new'];
+        var listItem = document.createElement('li');
+        var question = document.createElement('h1');
+        var question_time = document.createElement('span');
+        question.innerHTML = newQuestion['title'];
+        question.className = "title";
+        question_time.innerHTML = newQuestion['time'];
+        question_time.className = "time";
+        listItem.appendChild(question);
+        listItem.appendChild(question_time);
+        document.getElementById('questions').appendChild(listItem);
+    }else{
+      alert("bad message");
+    }
+  });
+
+  function addQuestion(){
+
+  }
