08f75346e3f0b98ae7101dad74334f7555b08b87
Kiran Ryali <kiran.ryali@gmail.com>
2011-02-24 01:32:16 -0600

    added config folder

---
+++ config/init.js
@@ -0,0 +1,6 @@
+//client = app.client = require("redis").createClient();
+questions = app.questions = [];
+
+var time = new Date();
+questions.push({title:"What is the meaning of life?", time: time.getTime(), vote: 1});
+questions.push({title:"What room is your computer in?", time: time.getTime(), vote: 1});
